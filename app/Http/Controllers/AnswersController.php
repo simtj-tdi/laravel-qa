e47f6a4bd1265528316b5eba89578183b250c2af
simtj-tdi <60678325+simtj-tdi@users.noreply.github.com>
2020-05-11 03:42:31 +0900

    Implement update answer

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -26,7 +26,6 @@ class AnswersController extends Controller
      */
     public function store(Question $question, Request $request)
     {
-
         $question->answers()->create($request->validate([
                 'body' => 'required'
             ]) + ['user_id' => \Auth::id()]);
@@ -40,9 +39,11 @@ class AnswersController extends Controller
      * @param  \App\Answer  $answer
      * @return \Illuminate\Http\Response
      */
-    public function edit(Answer $answer)
+    public function edit(Question $question, Answer $answer)
     {
-        //
+        $this->authorize('update', $answer);
+
+        return view('answers.edit', compact('question', 'answer'));
     }
 
     /**
@@ -52,9 +53,15 @@ class AnswersController extends Controller
      * @param  \App\Answer  $answer
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Answer $answer)
+    public function update(Request $request, Question $question, Answer $answer)
     {
-        //
+        $this->authorize('update', $answer);
+
+        $answer->update($request->validate([
+            'body' => 'required',
+        ]));
+
+        return redirect()->route('questions.show', $question->slug)->with('success', 'Your answer has been updated');
     }
 
     /**
